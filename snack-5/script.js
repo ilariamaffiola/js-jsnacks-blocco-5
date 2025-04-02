ad4bca2e0b14d0c84fbdc15e9eee81a6dbbf2530
Ilaria <maffiolailaria1@gmail.com>
2025-04-02 15:35:57 +0200

    [UPD]Filter even numbers from the array and log the result

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,11 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const evenNums = nums.filter((number)=>{
+    if(number%2 == 0){
+        return true;
+    }
+    return false;
+})
+console.log(evenNums);
 // Risultato: [2, 8, 4, 12]
